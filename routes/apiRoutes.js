8b8c5c9027f624847dcc97c8a84a679446f3faab
Mike D <michaelp.dwyer@gmail.com>
2019-07-13 14:16:57 -0700

    Adding Passport authentication info, updated HTML + JS files for login/signup/members

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,5 @@
 var db = require("../models");
-
+var passport = require("../config/passport");
 module.exports = function(app) {
   // Get all examples
   app.get("/api/examples", function(req, res) {
@@ -7,12 +7,41 @@ module.exports = function(app) {
       res.json(dbExamples);
     });
   });
-
+  // Using the passport.authenticate middleware with our local strategy.
+  // If the user has valid login credentials, send them to the members page.
+  // Otherwise the user will be sent an error
+  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+    res.json("/members");
+  });
   // Create a new example
   app.post("/api/examples", function(req, res) {
     db.Example.create(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
+  });
+
+  // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
+  // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
+  // otherwise send back an error
+  app.post("/api/signup", function(req, res) {
+    db.User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(function() {
+        res.redirect(307, "/api/login");
+      })
+      .catch(function(err) {
+        res.status(401).json(err);
+      });
+  });
+
+  // Route for logging user out
+  app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
   });
 
   // Delete an example by id
@@ -22,5 +51,21 @@ module.exports = function(app) {
     ) {
       res.json(dbExample);
     });
+  });
+
+  app.get("/api/user_data", function(req, res) {
+    if (!req.user) {
+      // The user is not logged in, send back an empty object
+      res.json({});
+    } else {
+      // Otherwise send back the user's email and id
+      // Sending back a password, even a hashed password, isn't a good idea
+      res.json({
+        firstName: req.user.firstName,
+        lastName: req.user.lastName,
+        email: req.user.email,
+        id: req.user.id
+      });
+    }
   });
 };
