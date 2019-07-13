06ef238b6a9b346e651ad8e866adde7730373e4b
Mike D <michaelp.dwyer@gmail.com>
2019-07-13 16:08:32 -0700

    Issues with Routes files

--- public/js/signup.js
+++ public/js/signup.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(email, password) {
+  function signUpUser(firstName, lastName, email, password) {
     $.post("/api/signup", {
       firstName: firstName,
       lastName: lastName,
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,9 @@
 var db = require("../models");
+var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
+  app.get("/game", isAuthenticated, function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
         msg: "Welcome!",
@@ -23,7 +24,7 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
+  // app.get("*", function(req, res) {
+  //   res.render("404");
+  // });
 };
