513adf964c3aa7d99f80d409f2c82230ec2f925a
SamanthaAO <samanthaaorcutt@gmail.com>
2019-07-14 12:16:49 -0700

    created login capability

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,38 @@
+// Requiring path to so we can use relative routes to our HTML files
+var path = require("path");
+
+// Requiring our custom middleware for checking if a user is logged in
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
 var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/game");
+      db.Example.findAll({}).then(function(dbExamples) {
+        res.render("index", {
+          msg: "Welcome!",
+          examples: dbExamples
+        });
+      });
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
+
+  app.get("/login", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/game");
+    }
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
+
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get("/game", isAuthenticated, function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
         msg: "Welcome!",
